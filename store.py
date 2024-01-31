4f42e476b1df5412e5c2b3e551dfcdde450061a7
Yonatan Dvir <64538919+yonatan-dvir@users.noreply.github.com>
2024-01-31 18:59:59 +0200

    Added sorting function to sort the search results of search_by_name and search_by_hashtag methods

--- store.py
+++ store.py
@@ -17,18 +17,31 @@ class Store:
                      item['description'])
                 for item in items_raw]
 
+    # Returns a list of the store items
     def get_items(self) -> list:
         return self._items
 
     # Returns a sorted list of all the items that match the search name (or contain it)
     def search_by_name(self, item_name: str) -> list:
+        # Not include items which are already in the current shopping cart
         matching_names = [cart_item for cart_item in self._items if cart_item.name.__contains__(item_name)]
+        self.sort_search_results(matching_names)
         return matching_names
 
     # Returns a sorted list of all the items matching the searches hashtag
     def search_by_hashtag(self, hashtag: str) -> list:
-        matching_hashtags = [cart_item for cart_item in self._items if cart_item.hashtags.__contains__(hashtag)]
+        matching_hashtags = [cart_item for cart_item in self._items if hashtag in cart_item.hashtags]
+        self.sort_search_results(matching_hashtags)
         return matching_hashtags
+
+    # Sort the search results
+    def sort_search_results(self, search_results: list):
+        # Not include items which are already in the current shopping cart
+        search_results = [result for result in search_results if result not in self._shopping_cart.cart_items]
+        cart_items = self._shopping_cart.cart_items
+        cart_tags = set(tag for item in cart_items for tag in item.hashtags)
+        # Order the search results based on the number of common hashtags and lexicographic order of names
+        search_results.sort(key=lambda item: (len(set(item.hashtags) & cart_tags), item.name))
 
     def add_item(self, item_name: str):
         # TODO: Complete
